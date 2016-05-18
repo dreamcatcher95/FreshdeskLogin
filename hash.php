da4048c66a7bb34634abbcee1a17303bc28640b6
rony <ronyislam95@yahoo.com>
2016-05-18 16:44:35 -0400

    new err

--- hash.php
+++ hash.php
@@ -20,24 +20,20 @@ if ($bind = ldap_bind($ldap, $username, $password)) {
     $number_returned = ldap_count_entries($ldap,$search);
     $info = ldap_get_entries($ldap, $search);
 
-  echo "The number of entries returned is ". $number_returned."<p>";
-
-for ($i=0; $i<$info["count"]; $i++) {
-   echo "Name is: ". $info[$i]["name"][0]."<br>";
-    $strName = $info[$i]["name"][0];
-   //echo "Display name is: ". $info[$i]["displayname"][0]."<br>";
-   echo "Email is: ". $info[$i]["mail"][0]."<br>";
-    $strEmail = $info[$i]["mail"][0];
-   //echo "Telephone number is: ". $info[$i]["telephonenumber"][0]."<p>";
-}
+    for ($i=0; $i<$info["count"]; $i++) {
+        $strName = $info[$i]["name"][0];
+        $strEmail = $info[$i]["mail"][0];
+    }
+    
+    function getSSOUrl($strName, $strEmail) {
+        $timestamp = time();
+        $to_be_hashed = $strName . "b19973a7c15a49e87120bff94d57785d" . $strEmail . $timestamp;
+        $hash = hash_hmac('md5', $to_be_hashed, "b19973a7c15a49e87120bff94d57785d");
+        return "http://northmillef.freshdesk.com/" . "login/sso/?name=".urlencode($strName)."&email=".urlencode($strEmail)."&timestamp=".$timestamp."&hash=".$hash;
+    }
+    header("Location: ".getSSOUrl($strName,$strEmail)); 
 } else {
-  print_r("fail");
+    $message = "Oops! You've put in the wrong userid/password.";
+    echo "<script type='text/javascript'>alert('$message');</script>";
 }
 
-function getSSOUrl($strName, $strEmail) {
-	$timestamp = time();
-	$to_be_hashed = $strName . "b19973a7c15a49e87120bff94d57785d" . $strEmail . $timestamp;
-	$hash = hash_hmac('md5', $to_be_hashed, "b19973a7c15a49e87120bff94d57785d");
-	return "http://northmillef.freshdesk.com/" . "login/sso/?name=".urlencode($strName)."&email=".urlencode($strEmail)."&timestamp=".$timestamp."&hash=".$hash;
-}
-header("Location: ".getSSOUrl($strName,$strEmail)); 
